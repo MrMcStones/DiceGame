66d2b9ec7834dccda2fb9b5388c67439c0731bf9
MrMcStones <rasmus.ke.nilsson@gmail.com>
2023-10-05 09:14:10 +0200

    TODO filled in

--- src/com/rasmus/dicegame/Main.java
+++ src/com/rasmus/dicegame/Main.java
@@ -1,4 +1,27 @@
 package com.rasmus.dicegame;
 
+/* TODO
+Amount of players - VG
+Amount of dice
+Player Name
+
+RULES
+Every player gets to throw one or more dice per turn
+The player with the highest score wins (minimum 2 players)
+
+VG
+
+Two winners, handle that
+Scanner in its own class
+Make sure the right answer through input
+Name can only be 'String'
+Number can only be 'Integer'
+Three relevant unit testings
+ */
+
 public class Main {
+
+    public static void main(String[] args) {
+
+    }
 }
