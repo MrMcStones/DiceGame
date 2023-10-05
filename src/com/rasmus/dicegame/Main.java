16dbc7c732f58e1985e8bfee6605ff085eed5092
MrMcStones <rasmus.ke.nilsson@gmail.com>
2023-10-05 10:44:44 +0200

    Added some input readings

--- src/com/rasmus/dicegame/Main.java
+++ src/com/rasmus/dicegame/Main.java
@@ -1,5 +1,6 @@
 package com.rasmus.dicegame;
 
+// TODO
 /* TODO
 Amount of players - VG
 Amount of dice
@@ -26,13 +27,27 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        Player player1 = new Player();
+        Player players = new Player("", 0, 0);
+        Player player1 = new Player("", 0,0);
+        Player player2 = new Player("", 0,0);
 
-        System.out.println("Enter how many players will compete");
-        System.out.println("Enter how many dice you want to throw every round");
-
+        System.out.println("Enter how many players will compete:");
+        players.user = sc.nextInt();
+        System.out.println("Enter how many dice you want to throw every round:");
+        players.die = sc.nextInt();
 
         System.out.println("Player 1 name:");
+        player1.name = sc.next();
         System.out.println("Player 2 name:");
+        player2.name = sc.next();
     }
 }
+
+// Checklist
+/* Checklist
+Scanner DONE - FIX into separate class
+Player clas DONE - constructor DONE
+Ask how many players DONE
+Ask how many die DONE
+FIX if or switch case for answer regarding players and die
+ */
--- src/com/rasmus/dicegame/Player.java
+++ src/com/rasmus/dicegame/Player.java
@@ -3,4 +3,12 @@ package com.rasmus.dicegame;
 public class Player {
 
     public String name;
+    int user;
+    int die;
+
+    public Player(String name, int user, int die) {
+        this.name = name;
+        this.user = user;
+        this.die = die;
+    }
 }
